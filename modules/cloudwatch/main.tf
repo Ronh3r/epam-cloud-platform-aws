bc459d11e7a937b1c9442894e74f9d0c4fe56c88
Aaron <aaronhdezbanc@gmail.com>
2025-11-14 00:07:48 -0600

    Fix cloudwatch module to remove duplicated log groups

--- modules/cloudwatch/main.tf
+++ modules/cloudwatch/main.tf
@@ -1,23 +1,23 @@
 # 1. Grupo de Logs para la Producer Lambda
 resource "aws_cloudwatch_log_group" "producer_log_group" {
-  name              = "/aws/lambda/${var.producer_lambda_name}-${var.module_environment}"
+  name              = "/aws/lambda/${var.producer_lambda_name}"
   retention_in_days = var.log_retention_days
 
   tags = {
-    Name        = "${var.producer_lambda_name}-logs-${var.module_environment}"
-    Project     = "${var.producer_lambda_name}-bookapi-${var.module_environment}"
+    Name        = "${var.producer_lambda_name}-logs"
+    Project     = "${var.producer_lambda_name}-bookapi"
     environment = var.module_environment
   }
 }
 
 # 2. Grupo de Logs para la Consumer Lambda
 resource "aws_cloudwatch_log_group" "consumer_log_group" {
-  name              = "/aws/lambda/${var.consumer_lambda_name}-${var.module_environment}"
+  name              = "/aws/lambda/${var.consumer_lambda_name}"
   retention_in_days = var.log_retention_days
 
   tags = {
-    Name        = "${var.consumer_lambda_name}-logs-${var.module_environment}"
-    Project     = "${var.consumer_lambda_name}-bookapi-${var.module_environment}"
+    Name        = "${var.consumer_lambda_name}-logs"
+    Project     = "${var.consumer_lambda_name}-bookapi"
     environment = var.module_environment
   }
 }
